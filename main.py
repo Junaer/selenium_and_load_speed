1de0bde8d941d7f347852080f9ce20e1e2125819
Evgenei <junaers1996@gmail.com>
2022-05-24 02:49:28 +0300

    second commit

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.chrome.service import Service
 import os.path
 import time
 import urllib.request
