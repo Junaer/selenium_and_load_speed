934d17f9d616d65a40629f873d042ab04b500d8d
gogelgans <gogelgans300@gmail.com>
2022-06-03 11:07:18 +0300

    feat: add OOP style

--- main.py
+++ main.py
@@ -9,67 +9,85 @@ import urllib.request
 
 driver = webdriver.Chrome('/home/gogelgans/Документы/work/homework/selenium_and_load_speed/chromedriver')
 
-def selenium_run_for_git(path, name):
-    driver.get(url='https://git-scm.com/')
-    download_page = driver.find_element(by=By.XPATH, value='//*[@id="nav-downloads"]/a/h3')
-    download_page.click()
-    win_page = driver.find_element(by=By.XPATH, value='//*[@id="main"]/div[1]/div[1]/div/table/tbody/tr[1]/td[2]/a')
-    win_page.click()
-    download_git = driver.find_element(by=By.XPATH, value='//*[@id="main"]/p[3]/strong/a')
-    start = time.perf_counter()
-    download_git.click()
-    while True:
-        download_folder = os.path.expanduser(path)
-        filenames = glob.glob(download_folder + name)
-        if len(filenames) > 0 and not any('.crdownload' in name for name in filenames):
-            break
-        for name in filenames:
-            if name.endswith('.crdownload'):
-                continue
-            if name.endswith('.xls'):
-                print('')
-                print('Download complete.')
-                print('')
+class selenium_download():
+    def __init__(self, path, name):
+        self.path = path
+        self.name = name
+
+    def selenium_run_for_git(self):
+        driver.get(url='https://git-scm.com/')
+        download_page = driver.find_element(by=By.XPATH, value='//*[@id="nav-downloads"]/a/h3')
+        download_page.click()
+        win_page = driver.find_element(by=By.XPATH, value='//*[@id="main"]/div[1]/div[1]/div/table/tbody/tr[1]/td[2]/a')
+        win_page.click()
+        download_git = driver.find_element(by=By.XPATH, value='//*[@id="main"]/p[3]/strong/a')
+        start = time.perf_counter()
+        download_git.click()
+        while True:
+            download_folder = os.path.expanduser(self.path)
+            filenames = glob.glob(download_folder + self.name)
+            if len(filenames) > 0 and not any('.crdownload' in self.name for self.name in filenames):
                 break
-            else:
+            for self.name in filenames:
+                if self.name.endswith('.crdownload'):
+                    continue
+                if self.name.endswith('.xls'):
+                    print('')
+                    print('Download complete.')
+                    print('')
+                    break
+                else:
+                    break
+        run_time = time.perf_counter() - start
+        print(run_time)
+        return round(run_time, 2)
+
+    def selenium_run_for_7zip(self):
+        driver.get(url='https://www.7-zip.org')
+        download_7zip = driver.find_element(by=By.XPATH, value='/html/body/table/tbody/tr/td[2]/table/tbody/tr/td[1]/table[1]/tbody/tr[2]/td[1]/a')
+        start = time.perf_counter()
+        download_7zip.click()
+        while True:
+            download_folder = os.path.expanduser(self.path)
+            filenames = glob.glob(download_folder + self.name)
+            if len(filenames) > 0 and not any('.crdownload' in self.name for self.name in filenames):
                 break
-    run_time = time.perf_counter() - start
-    print(run_time)
-    return round(run_time, 2), path, name
+            for self.name in filenames:
+                if self.name.endswith('.crdownload'):
+                    continue
+                if self.name.endswith('.xls'):
+                    print('')
+                    print('Download complete.')
+                    print('')
+                    break
+                else:
+                    break
+        run_time = time.perf_counter() - start
+        print(run_time)
+        return round(run_time, 2)
+        # download_7zip = driver.find_element(by=By.XPATH, value='//*[@id="sidebar"]/nav/ul/li[1]/a')
+        # start = time.perf_counter()
+        # download_7zip.click()
+        # run_time = time.perf_counter() - start
+        # driver.switch_to.window(driver.window_handles[0])
+        # driver.close()
+        # return round(run_time, 2)
 
-def selenium_run_for_7zip():
-    driver.get(url='https://www.google.ru/')
-    search = driver.find_element(by=By.XPATH, value='/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input')
-    search.send_keys('скачать zip 7', Keys.ENTER)
-    link_7zip = driver.find_element(by=By.XPATH, value='//*[@id="rso"]/div[1]/div/div[1]/div/a/div')
-    link_7zip.click()
-    # download_7zip = driver.find_element(by=By.XPATH, value='//*[@id="sidebar"]/nav/ul/li[1]/a')
-    # start = time.perf_counter()
-    # download_7zip.click()
-    # run_time = time.perf_counter() - start
-    driver.close()
-    driver.switch_to.window(driver.window_handles[0])
-    driver.close()
-    # return round(run_time, 2)
+    def get_avg_speed(self, func):
+        time_download = func
+        full_path = f'{self.path}{self.name}'
+        size = os.path.getsize(self.name)
+        avg_speed = size / time_download
+        round_avg_speed = round(avg_speed)
+        return print(f"Вес файла {size} байт'а. Cредняя скорость скачивания {round_avg_speed} байт'а в секунду или {round_avg_speed/1000000} мегабайт'а в секунду")
 
 
-# def download_file_and_check_time(name, url):
-#     name = name
-#     url = url
-#     start = time.perf_counter()
-#     urllib.request.urlretrieve(url, filename=name)
-#     run_time = time.perf_counter() - start
-#     return round(run_time, 2)
-
-def get_avg_speed(name, path, url):
-    time_download = download_file_and_check_time(name=name, url=url)
-    size = os.path.getsize(path)
-    avg_speed = size / time_download
-    round_avg_speed = round(avg_speed)
-    return print(f"Вес файла {size} байт'а. Cредняя скорость скачивания {round_avg_speed} байт'а в секунду")
 
 
 if __name__ == '__main__':
-    selenium_run_for_git('/home/gogelgans/Загрузки/', 'Git-2.36.1-64-bit.exe')
+    # download_git = selenium_download('/home/gogelgans/Загрузки/', 'Git-2.36.1-64-bit.exe')
+    # download_git.get_avg_speed(download_git.selenium_run_for_git())
+    download_7zip = selenium_download('/home/gogelgans/Загрузки/', '')
+    download_7zip.selenium_run_for_7zip()
 
 
